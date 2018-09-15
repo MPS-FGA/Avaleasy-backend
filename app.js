9c5fd7ab784a8985890213e1346c245bcf4f2fe8
Alax Alves <alaxallves@gmail.com>
2018-09-15 16:22:41 -0300

    Fixing linting errors

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var mongo = require('mongodb');
-var monk = require('monk');
-var db = monk('localhost:27017/avaleasy-db');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongo = require('mongodb');
+const monk = require('monk');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const db = monk('localhost:27017/avaleasy-db');
 
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,7 +23,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function(req, res, next){
+app.use((req, res, next) => {
   req.db = db;
   next();
 });
@@ -31,12 +32,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
