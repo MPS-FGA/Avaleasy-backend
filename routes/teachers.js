4b434c5790f580057cbac9d5a08c9e84d10e63fd
IagoCarvalho <IagoVC_2012@hotmail.com>
2018-09-16 15:21:12 -0300

    Adding mongoose config for mongo connection
    Co-authored-by: matHeuzin <matheusbattista@hotmail.com>

--- routes/teachers.js
+++ routes/teachers.js
@@ -1,5 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://db:27017/base');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
 
 /* POST teachers. */
 router.post('/', (req, res, next) => {
