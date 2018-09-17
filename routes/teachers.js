60a1c9b27dd15e28da3ef5ed982abb7e97ba5abf
IagoCarvalho <IagoVC_2012@hotmail.com>
2018-09-17 13:21:57 -0300

    Creating function to use hash in teacher's password
    Co-authored-by: matHeuzin <matheusbattista@hotmail.com>

--- routes/teachers.js
+++ routes/teachers.js
@@ -1,33 +1,65 @@
 var express = require('express');
 var router = express.Router();
+var crypto = require('crypto');
+
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://db:27017/base');
-
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   // we're connected!
 });
-var Schema = mongoose.Schema;
 
+var Schema = mongoose.Schema;
 var teacherDataSchema = new Schema({
-  nome: {type:String, required:true},
-  email: {type:String, required:true}
+  name: {type:String, required:true},
+  email: {type:String, required:true},
+  password: {type:String, required:true},
+  hash: String,
+  salt: String
 }, {collection: 'teachers'});
 
 var Teachers = mongoose.model('TeacherData', teacherDataSchema);
 
+/* generates salt for hash with random char string*/
+var genRandomString = function(length){
+  return crypto.randomBytes(Math.ceil(length/2))
+          .toString('hex')
+          .slice(0,length);
+};
+
+/* hash password with sha512 */
+var sha512 = function(password, salt){
+  var hash = crypto.createHmac('sha512', salt);
+  hash.update(password);
+  var value = hash.digest('hex');
+  return {
+      salt:salt,
+      passwordHash:value
+  };
+};
+
+function hashPassword(password) {
+  var salt = genRandomString(16);
+  var passwordData = sha512(password, salt);
+
+  return passwordData;
+}
+
 /* POST teachers. */
-router.post('/novo', function(req, res, next) {
- var teacher = {
-   nome: req.body.nome,
-   email: req.body.email
- };
+router.post('/new', function(req, res, next) {
+  var teacher = {
+   name: req.body.nome,
+   email: req.body.email,
+   password: req.body.password
+  };
 
- var data = new Teachers(teacher);
- data.save();
+  teacher['password'] = hashPassword(teacher['password']);
 
- res.redirect('/');
+  var data = new Teachers(teacher);
+  data.save();
+
+  res.redirect('/');
 });
 
 module.exports = router;
