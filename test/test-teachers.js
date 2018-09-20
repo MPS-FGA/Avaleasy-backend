2ee6728fbb11d9be4f9399069050233e7b20db00
IagoCarvalho <IagoVC_2012@hotmail.com>
2018-09-19 23:09:22 -0300

    Adding tests for teacher creation

---
+++ test/test-teachers.js
@@ -0,0 +1,33 @@
+const chai = require('chai');
+const { expect } = require('chai');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://db:27017/base');
+
+chai.use(require('chai-http'));
+
+const app = require('../app.js');
+
+describe('Api users', function describe() {
+  this.timeout(10000); // How long to wait for a response
+
+  before(() => {
+
+  });
+
+  afterEach((done) => {
+    mongoose.connection.db.dropCollection('teachers');
+    done();
+  });
+
+  it('should add a SINGLE Teacher on /teachers/new POST', (done) => {
+    chai.request(app)
+      .post('/teachers/new')
+      .send({ name: 'Professor', email: 'Professor@unbmail.com', password: '123' })
+      .then((res) => {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        done();
+      });
+  });
+});
