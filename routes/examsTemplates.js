0d50243b849ad18de64a282838121324141f301f
IagoCarvalho <IagoVC_2012@hotmail.com>
2018-10-02 19:11:13 -0300

    fixing exams models and adding tests

--- routes/examsTemplates.js
+++ routes/examsTemplates.js
@@ -1,51 +1,7 @@
 const express = require('express');
+const ExamsTemplates = require('../models/examTemplate.js');
+
 const router = express.Router();
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://db:27017/base');
-const { Schema } = mongoose;
-
-const tblQuestions = new Schema({
-  title: { type: String, required: true },
-  punctuation: { type: Number },
-  alternative1Content: { type: String, required: true },
-  alternative2Content: { type: String, required: true },
-  alternative3Content: { type: String, required: true },
-  alternative4Content: { type: String, required: true },
-});
-
-const objectiveQuestions = new Schema({
-  title: { type: String, required: true },
-  punctuation: { type: Number },
-});
-
-const alternatives = new Schema({
-  content: { type: String, required: true }
-});
-
-const multipleChoiceQuestions = new Schema({
-  title: { type: String, required: true },
-  punctuation: { type: Number },
-  numberOfAlternatives: { type: Number, required: true, default: 4},
-  multipleChoiceAlternatives: [alternatives],
-});
-
-const tfQuestions = new Schema({
-  title: { type: String, required: true },
-  punctuation: { type: Number },
-  value: { type: Boolean, required: true },
-});
-
-const ExamTemplate = new Schema({
-  title: { type: String, required: true, unique: true},
-  value: { type: Number, min: 0, max:100, default: 100, required: true},
-  tblQuestions: [tblQuestions],
-  objectiveQuestions: [objectiveQuestions],
-  multipleChoiceQuestions: [multipleChoiceQuestions],
-  tfQuestions: [tfQuestions],
-}, { collection: 'examsTemplates'});
-
-const ExamsTemplates = mongoose.model('ExamTemplateData', ExamTemplate);
 
 /* POST Exam Model */
 router.post('/new', (req, res, next) => {
@@ -73,7 +29,6 @@ router.post('/new', (req, res, next) => {
       success: true,
     });
   });
-
 });
 
 module.exports = router;
