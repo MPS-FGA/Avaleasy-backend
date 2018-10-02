cfa5463d79335814d8687810cd4f03262c131322
IagoCarvalho <IagoVC_2012@hotmail.com>
2018-10-01 23:10:45 -0300

    Adding exams templates schemas and post method for testing

---
+++ routes/examsTemplates.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://db:27017/base');
+const { Schema } = mongoose;
+
+const tblQuestions = new Schema({
+  title: { type: String, required: true },
+  punctuation: { type: Number },
+});
+
+const objectiveQuestions = new Schema({
+  title: { type: String, required: true },
+  punctuation: { type: Number },
+});
+
+const multipleChoiceQuestions = new Schema({
+  title: { type: String, required: true },
+  punctuation: { type: Number },
+  numberOfAlternatives: { type: Number, required: true, default: 4},
+});
+
+const tfQuestions = new Schema({
+  title: { type: String, required: true },
+  punctuation: { type: Number },
+  value: { type: Boolean, required: true },
+});
+
+const ExamTemplate = new Schema({
+  title: { type: String, required: true, unique: true},
+  value: { type: Number, min: 0, max:100, default: 100, required: true},
+  tblQuestions: [tblQuestions],
+  objectiveQuestions: [objectiveQuestions],
+  multipleChoiceQuestions: [multipleChoiceQuestions],
+  tfQuestions: [tfQuestions],
+}, { collection: 'examsTemplates'});
+
+const ExamsTemplates = mongoose.model('ExamTemplateData', ExamTemplate);
+
+/* POST Exam Model */
+router.post('/new', (req, res, next) => {
+  const examTemplate = {
+    title: req.body.title,
+    value: req.body.value,
+  };
+
+  const data = new ExamsTemplates(examTemplate);
+
+  data.save((err) => {
+    if (err) {
+      if (err.name === 'MongoError' && err.code === 11000) {
+        // Duplicate exam template
+        return res.status(500).send({ succes: false, message: 'This template already exists!' });
+      }
+      // Some other error
+      return res.status(500).send(err);
+    }
+    return res.json({
+      success: true,
+    });
+  });
+
+});
+
+module.exports = router;
