9a4bcc0433e9db6856c0c05ca1a6b8c1cddc64d2
IagoCarvalho <IagoVC_2012@hotmail.com>
2018-10-04 22:23:00 -0300

    Adding tests to exams templates get methods

--- test/test-examsTemplates.js
+++ test/test-examsTemplates.js
@@ -2,6 +2,7 @@ const chai = require('chai');
 const { expect } = require('chai');
 const mongoose = require('mongoose');
 
+const ExamsTemplates = require('../models/examTemplate.js');
 
 mongoose.connect('mongodb://db:27017/test');
 
@@ -9,8 +10,26 @@ chai.use(require('chai-http'));
 
 const app = require('../app.js');
 
-describe('Api users', function describe() {
-  this.timeout(10000); // How long to wait for a response
+const examMockData = {
+  title: 'Modelo de avaliacao Objetiva',
+  value: 100,
+  tblQuestions: [],
+  objectiveQuestions: [
+    {
+      title: 'Questao objetiva',
+      punctuation: 2,
+    },
+    {
+      title: 'Outra Questao objetiva',
+      punctuation: 3,
+    },
+  ],
+  multipleChoiceQuestions: [],
+  tfQuestions: [],
+};
+
+describe('Exams Templates POST tests', function describe() {
+  this.timeout(1000000000); // How long to wait for a response
 
   before(() => {
 
@@ -24,23 +43,7 @@ describe('Api users', function describe() {
   it('should add a SINGLE exam template on /examsTemplates/new POST', (done) => {
     chai.request(app)
       .post('/examsTemplates/new')
-      .send({
-        title: 'Modelo de avaliacao Objetiva',
-        value: 100,
-        tblQuestions: [],
-        objectiveQuestions: [
-          {
-            title: 'Questao objetiva',
-            punctuation: 2,
-          },
-          {
-            title: 'Outra Questao objetiva',
-            punctuation: 3,
-          },
-        ],
-        multipleChoiceQuestions: [],
-        tfQuestions: [],
-      })
+      .send(examMockData)
       .end((err, res) => {
         expect(res).to.have.status(200);
         expect(res).to.be.json;
@@ -54,6 +57,65 @@ describe('Api users', function describe() {
       .send({ title: '', value: '' })
       .end((err, res) => {
         expect(res).to.have.status(400);
+        done();
+      });
+  });
+});
+
+describe('Exams Templates GET tests', function describe() {
+  this.timeout(1000000000); // How long to wait for a response
+
+  before(() => {
+
+  });
+
+  afterEach((done) => {
+    mongoose.connection.db.dropDatabase();
+    done();
+  });
+
+  it('Should return a list of exams templates on /examsTemplates/', (done) => {
+    const examTemplate = new ExamsTemplates(examMockData);
+    examTemplate.save();
+
+    chai.request(app)
+      .get('/examsTemplates/')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an('array');
+        expect(res.body[0].title).to.equal('Modelo de avaliacao Objetiva');
+
+        done();
+      });
+  });
+
+  it('Should return a single exam template on /examsTemplates/:id', (done) => {
+    const examTemplate = new ExamsTemplates(examMockData);
+    examTemplate.save();
+
+    const url = `/examsTemplates/${examTemplate.id}`;
+
+    chai.request(app)
+      .get(url)
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an('object');
+
+        done();
+      });
+  });
+
+  it('Should not return a exam template on /examsTemplates/:id with invalid id', (done) => {
+    const examTemplate = new ExamsTemplates(examMockData);
+    examTemplate.save();
+
+    const url = '/examTemplate/4';
+
+    chai.request(app)
+      .get(url)
+      .end((err, res) => {
+        expect(res).to.have.status(404);
+
         done();
       });
   });
