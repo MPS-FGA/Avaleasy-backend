bf8906dc585a623cd809c89bad6096bca2219803
Andre Filho <andre.filho001@outlook.com>
2018-10-21 15:04:12 -0300

    Fixed method at /auth/sign-in
    Co-authored-by: alax alves <alaxallves@gmail.com>

--- routes/auth.js
+++ routes/auth.js
@@ -1,45 +1,40 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
-const Teachers = require('../models/teacher');
-
-// const app = require('../app');
-// const hashPassword = require('../utils/password');
+const bcrypt = require('bcrypt');
+const Teacher = require('../models/teacher');
+const router = express();
 
 mongoose.connect('mongodb://db:27017/base');
 
-const router = express();
-
 // POST /auth/sign-in
 router.post('/sign-in', (req, res, next) => {
-  Teachers.findOne({ email: req.body.email })
-    .then((teacher) => {
-      if (!teacher) {
-        return res.status(401).json({
-          message: 'Auth failed'
+  Teacher.findOne({ email: req.body.email })
+    .then(user => {
+      if (!user) {
+        return res.json(404, {
+          msg: "user not found"
         });
       }
-      return bcrypt.compare(req.body.password, teacher.password);
-    })
-    .then(result => {
-      if (!result) {
-        return res.status(401).json({
-          message: "auth failed",
-        })
-      }
-      const token = jwt.sign(
-        {email: teacher.email, userId: teacher._id},
-        'secretkey',
-        { expiresIn: '1h' }
-      );
-      res.statusCode(200).json({
-        token,
-      });
-    })
-    .catch((err) => {
-      res.json({ err });
+      bcrypt.compare(req.body.password, user.password, (result, err) => {
+        if (!result) {
+          return res.json(401, {
+            error: "Wrong credentials",
+          });
+        }
+        if (err) {
+          return res.json(500, { err });
+        }
+        const token = jwt.sign(
+          { teacherId: user._id, name: user.name, email: user.email },
+          'secretkey',
+          { expiresIn: '1h' }
+        );
+        res.json(200, {
+          token,
+        });
+      })
     });
 });
-
 
 module.exports = router;
